2a7d99515fddebd536b17bfa993a9d9bb0e849a5
stiigp <miltonfine33@gmail.com>
2024-08-30 10:03:31 -0300

    Desenvolvendo componentes de layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import Pagina from './componentes/layouts/Pagina';
+
 function App() {
   return (
     <div className="App">
-      
+      <Pagina>
+        <h1>Esta é a página de cadastro de cliente</h1>
+      </Pagina>
+      <Pagina>
+        <h1>Esta é a página de cadastro de produto</h1>
+      </Pagina>
+      <Pagina>
+        <h1>Esta é a página de cadastro de fornecedor</h1>
+      </Pagina>
     </div>
   );
 }
